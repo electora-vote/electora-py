d39d57b483bf4126053c8a26f87a3eabff4c0801
Kieran Prasch <kieranprasch@gmail.com>
2023-06-29 11:16:16 +0200

    download and decrypt votes

--- electora_py/api.py
+++ electora_py/api.py
@@ -18,6 +18,7 @@ _COORDINATOR_URI = "https://polygon-mumbai.infura.io/v3/a11313ddcf61443898b6a47e
 _TEACHER_URI = "https://lynx.nucypher.network:9151"
 _ELECTORA_ARWEAVE_TAG = "ballot_uuid"
 _ARWEAVE_GQL_ENDPOINT = "https://devnet.bundlr.network/graphql"
+_ARWEAVE_DATA_ENDPOINT = "https://arweave.net/"
 _GET_ELECTION_VOTES_QUERY_TEMPLATE = """
 query getElectionVotes ($tagName: String!, $electionId: String!) {
     transactions(tags: [{ name: $tagName, values: [$electionId] }]) {
@@ -56,10 +57,36 @@ def _fetch_vote_transactions(
     return result
 
 
-def fetch_votes(election_id: str, endpoint: str = _ARWEAVE_GQL_ENDPOINT):
-    transactions = _fetch_vote_transactions(election_id=election_id, endpoint=endpoint)
-    raise NotImplementedError(transactions)
+def _fetch_vote_data(transactions) -> List[str]:
+    edges = transactions["transactions"]["edges"]
+    vote_ciphertexts = list()
+    for transaction in edges:
+        transaction_id = transaction["node"]["id"]
+        response = requests.get(_ARWEAVE_DATA_ENDPOINT + transaction_id)
+        if response.status_code == 200:
+            vote_ciphertexts.append(response.text)
+        else:
+            raise Exception("Could not fetch vote data from arweave")
+    return vote_ciphertexts
 
+
+def fetch_votes(election_id: str, endpoint: Optional[str] = _ARWEAVE_GQL_ENDPOINT) -> List[str]:
+    """Fetches and decrypts all votes for a particular election ID."""
+    connect_to_blockchain(eth_provider_uri=_GOERLI_URI, emitter=StdoutEmitter())
+    BOB.start_learning_loop(now=True)
+    transactions = _fetch_vote_transactions(election_id=election_id, endpoint=endpoint)
+    ciphertexts = _fetch_vote_data(transactions=transactions)
+    cleartexts = _decrypt_votes(vote_ciphertexts=ciphertexts)
+    return cleartexts
+
+
+def _decrypt_votes(vote_ciphertexts) -> List[str]:
+    """Decrypts a single encrypted vote."""
+    cleartexts = list()
+    for ciphertext in vote_ciphertexts:
+        cleartext = _decrypt_vote(ciphertext=ciphertext, timestamp=1687870663)
+        cleartexts.append(cleartext)
+    return cleartexts
 
 def _get_conditions(timestamp):
     time_condition = {
@@ -69,12 +96,11 @@ def _get_conditions(timestamp):
     return [time_condition]
 
 
-def decrypt_vote(ciphertext, timestamp):
-    connect_to_blockchain(eth_provider_uri=_GOERLI_URI, emitter=StdoutEmitter())
-    BOB.start_learning_loop(now=True)
+def _decrypt_vote(ciphertext, timestamp):
+    ciphertext = ferveo.Ciphertext.from_bytes(bytes.fromhex(ciphertext))
     cleartext = BOB.threshold_decrypt(
-        ritual_id=0,
-        ciphertext=ferveo.Ciphertext.from_bytes(bytes.fromhex(ciphertext)),
+        ritual_id=_RITUAL_ID,
+        ciphertext=ciphertext,
         conditions=_get_conditions(timestamp),
     )
     return bytes(cleartext).decode()
