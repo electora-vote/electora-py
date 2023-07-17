cacf139369c85cd65edecb62b69b3fe0513102ff
James Campbell <james.campbell@tanti.org.uk>
2023-07-17 13:03:57 +0200

    Apply Black

--- electora_py/cli.py
+++ electora_py/cli.py
@@ -9,7 +9,7 @@ app = typer.Typer()
 
 @app.command()
 def decrypt_vote(ciphertext: str, timestamp: datetime) -> str:
-    return api.decrypt_vote(ciphertext, timestamp)
+    return api._decrypt_vote(ciphertext, timestamp)
 
 
 if __name__ == "__main__":
